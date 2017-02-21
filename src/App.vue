158db3c78640401b4f9e22bc6a76d192f0fe3429
devjin0617 <devjin0617@gmail.com>
2017-02-21 21:33:24 +0900

    init with upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="app-image">
+      <img src="./assets/logo.png">
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.app-image {
+  text-align:center;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <div class="wrap">
+      <h1>{{ msg }}</h1>
+    </div>
+    <div>
+      <MonacoEditor
+        height='200'
+        language="typescript"
+        :code='code'
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import MonacoEditor from 'vue-monaco-editor'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to MonacoEditor example',
+      code: `var hello = 'world'\nconsole.log(hello)`
+    }
+  },
+  components: {
+    MonacoEditor
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.wrap {
+  text-align: center;
+}
+</style>
